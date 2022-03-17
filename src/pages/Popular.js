4214566c3b4ceab3dba74d7cf36a1e2b48abf8db
Adarsh Chakraborty <adarshchak108@gmail.com>
2022-03-17 10:56:11 +0530

    Splide Slider

--- src/pages/Popular.js
+++ src/pages/Popular.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/splide/dist/css/splide.min.css';
 
 const Popular = () => {
   const [popular, setPopular] = useState([]);
@@ -16,13 +19,77 @@ const Popular = () => {
 
   return (
     <div>
-      {popular.map((receipe) => (
-        <div key={receipe.id}>
-          <p>{receipe.title}</p>
-        </div>
-      ))}
+      <Wrapper>
+        <h3>Popular Picks</h3>
+
+        <Splide
+          options={{
+            perPage: 4,
+            arrows: false,
+            pagination: false,
+            drag: 'free',
+            gap: '5rem'
+          }}
+        >
+          {popular.map((receipe) => {
+            return (
+              <SplideSlide key={receipe.id}>
+                <Card>
+                  <p>{receipe.title}</p>
+                  <img src={receipe.image} alt={receipe.title} />
+                  <Gradient />
+                </Card>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapper>
     </div>
   );
 };
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  position: relative;
+
+  img {
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 2rem;
+  }
+
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const Gradient = styled.div`
+z-index: 3
+position: absolute;
+width: 100%;
+height: 100%;
+background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5))`;
 
 export default Popular;
